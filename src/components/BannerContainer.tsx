819240a07b7532a4c49b044ceb8b188f6b5c5bdd
Asmaa Salem <asmaa.salem@tactful.ai>
2024-02-04 23:15:43 +0200

    add banner asset

--- src/components/BannerContainer.tsx
+++ src/components/BannerContainer.tsx
@@ -8,67 +8,52 @@ import '../styles/banner.scss'
 const Banners = () => {
 
     const [banners, setBanners] = useState<Banner[]>([]);
-    const [index, setIndex] = useState(0);
+    // const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex: any) => {
-      setIndex(selectedIndex);
-    };
+    // const handleSelect = (selectedIndex: any) => {
+    //   setIndex(selectedIndex);
+    // };
 
     useEffect(() => {
         // get highlited banners 
 
-        fetch(`${API_URL}fee177346e7875554413`)
-        .then(res => res.json())
-        .then(response => {            
-            setBanners(response.slides)
+        // fetch(`${API_URL}fee177346e7875554413`)
+        // .then(res => res.json())
+        // .then(response => {            
+        //     setBanners(response.slides)
+        // })
+
+        fetch('/banners.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
         })
+        .then(data => setBanners(data.slides))
+        .catch(error => console.error('There has been a problem with your fetch operation:', error));
         
 
     }, []);
     return ( <section>
-
-{/* <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    */}
         <Carousel> 
             {/* TODO: keep display by order */}
             {banners.map(banner => {
                 return  <Carousel.Item key={banner.id}>
                 {/* TODO: keep display by order */}
                 <Carousel.Caption>
-                    <div className="d-flex gap-5">
-                    <div>
+                    <div className="d-flex justify-content-center">
+                    <div className="py-5" style={{width: 600}}>
                         <h5 className="banner-category" style={{color: `#${banner.colorCode}`}}> {banner.category} </h5>
                         <h1>{banner.title}</h1>
                         <p>{banner.brief} </p>
 
                         {/* TODO: link to urls / actions  */}
                         <Stack direction="horizontal" gap={2} className="mt-5">
-                            <Button size="lg" className="btn-orange" > Find out more </Button>
-                            <Button size="lg" className="btn-video" > Play Demo </Button> 
+                            <Button size="lg" className="btn-orange" target='_blank' href={banner.itemUrl} > Find out more </Button>
+                            <Button size="lg" variant="link" className="btn-video" > Play Demo </Button> 
                         </Stack>
-                        </div>
+                    </div>
                         <img src={banner.imgUrl} alt={banner.title} />
 
                     </div>
@@ -76,7 +61,6 @@ const Banners = () => {
             </Carousel.Item>
             })}
         </Carousel>
-   
     </section> );
 }
  
