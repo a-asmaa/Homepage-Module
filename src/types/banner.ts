491d86eb0e5da2d5f5ab20648defe017fe1de952
Asmaa Salem <asmaa.salem@tactful.ai>
2024-02-04 17:05:56 +0200

    add types

---
+++ src/types/banner.ts
@@ -0,0 +1,11 @@
+export type Banner = {
+    id: number,
+    brief: string;
+    order: number;
+    title: string;
+    imgUrl: string;
+    itemUrl: string;
+    category: string;
+    videoUrl: string;
+    colorCode: string;
+}
---
+++ src/types/newsItem.ts
@@ -0,0 +1,17 @@
+export type NewsType = {
+    id: string;
+    title: string;
+    content: string;
+    categoryID: string;
+    urlToImage: string;
+    description: string;
+    publishedDate: string;
+    showOnHomepage: string;
+}
+
+export type Category = {
+    
+    id: number;
+    name: string;
+}
+
