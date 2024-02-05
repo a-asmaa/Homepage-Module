b76145fa6d29393b08f3fe5a0a23500cac9cb511
Asmaa Salem <asmaa.salem@tactful.ai>
2024-02-05 21:23:22 +0200

    things we do section

--- src/components/ThingsWeDo.tsx
+++ src/components/ThingsWeDo.tsx
@@ -1,13 +1,13 @@
 import '../styles/thingsWeDo.scss'
 import textImg from '../assets/Things _We Do.png';
 import pinkPath from '../assets/Pink-path.png';
-
-
+import { Stack } from 'react-bootstrap';
 import services from '../assets/services.png';
 import dynamics from '../assets/dynamics.png';
-// import transformation from '../assets/Things _We Do.png';
+import transformation from '../assets/Transformation.png';
 import envision from '../assets/envision.png';
 import craftyMind from '../assets/crafty-mind.png';
+
 
 const ThingsWeDo = () => {
     return ( <section className='things-do-section'>
@@ -16,27 +16,72 @@ const ThingsWeDo = () => {
         <div className='section-container'>
             <img className='pink-img' src={pinkPath} alt="path"/>
             
-            <div className='section-content col-5'>
-                <h1> We Deliver Digital Productivity </h1>
+            <div className='container ras'>
+                <div className='section-content col-5'>
+                    <h1> We Deliver Digital Productivity </h1>
 
-                <p>
-                    We craft technology solutions that digitally bond and transform the productivity of our customers and their citizens, workers, consumers and partners.
-                </p>
+                    <p>
+                        We craft technology solutions that digitally bond and transform the productivity of our customers and their citizens, workers, consumers and partners.
+                    </p>
+                </div>
+
+                <div className='section-content' style={{height: 'fit-content'}}>
+                    <div className='img-container'>
+                        <img src={transformation} alt="things_we_do"/>
+
+                        <Stack direction="horizontal" gap={3} className='read-more'>
+                            <i className="bi bi-arrow-right"></i>
+                            <span> Read more</span>
+                        </Stack> 
+                    </div>
+                    <div className='img-container'>
+                        <img src={transformation} alt="things_we_do"/>
+
+                        <Stack direction="horizontal" gap={3} className='read-more'>
+                            <i className="bi bi-arrow-right"></i>
+                            <span> Read more</span>
+                        </Stack> 
+                       
+                    </div>
+                </div>
+
+                <div className='section-content' >
+                    <div style={{height: 50}}></div>
+                    <div className='img-container' >
+                        <img src={transformation} alt="things_we_do"/>
+
+                        <Stack direction="horizontal" gap={3} className='read-more'>
+                            <i className="bi bi-arrow-right"></i>
+                            <span> Read more</span>
+                        </Stack> 
+                    </div>
+                    <div className='img-container' >
+                        <img src={transformation} alt="things_we_do"/>
+
+                        <Stack direction="horizontal" gap={3} className='read-more'>
+                            <i className="bi bi-arrow-right"></i>
+                            <span> Read more</span>
+                        </Stack> 
+                    </div>
+                </div>
+
+                <div className='section-content my-auto'>
+                    <div className='img-container'>
+                        <img src={transformation} alt="things_we_do"/>
+
+                        <Stack direction="horizontal" gap={3} className='read-more'>
+                            <i className="bi bi-arrow-right"></i>
+                            <span> Read more</span>
+                        </Stack> 
+                    </div>
+                </div>
+
             </div>
+            
         </div>
         <img className='text-img' src={textImg} alt="things_we_do"/>
 
-        <div className='img-container'>
-            <img src={craftyMind} alt="things_we_do"/>
-            <span className='text'> Crafty Mind </span> 
-            <span className='text'> Services </span>
-            
-            <span className='text'> Envision </span>
-            <span className='text'> Dynamics 365 </span>
-            <img src={services} alt="path"/> 
-            <img src={dynamics} alt="path"/> 
-            <img src={envision} alt="path"/> 
-        </div>
+        
         
     
         
